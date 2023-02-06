8b38529f45913fa8e03a0b243a582d1a47d63590
Valeriya <valeriyalukovkina@gmail.com>
2023-02-06 02:30:48 +0200

    ready

---
+++ vue-project/src/App.vue
@@ -0,0 +1,10 @@
+
+
+<template>
+  <!-- <header> -->
+    <!-- <router-link to="/">Go to Setting</router-link>
+    <router-link to="/game">Go to Game</router-link> -->
+    <!-- <h1>Hello</h1> -->
+  <!-- </header> -->
+  <router-view ></router-view>
+</template>
